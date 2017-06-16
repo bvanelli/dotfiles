b8e64fef0dc9d9c4c71356a2ee1f2f3e51e8005c
bvanelli <brunnovanelli@gmail.com>
2017-06-16 11:26:17 -0300

    Added setup file

---
+++ setup.sh
@@ -0,0 +1,3 @@
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+config config --local status.showUntrackedFiles no
+echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.bashrc
