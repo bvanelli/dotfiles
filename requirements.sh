8ef08b8449bee2e28c130b94028bbb43b75752de
Brunno <brunnovanelli@gmail.com>
2017-06-30 11:15:33 -0300

    Updated MATLAB icon local

---
+++ requirements.sh
@@ -0,0 +1,15 @@
+
+# Add ppas
+sudo add-apt-repository ppa:snwh/pulp
+sudo apt-get update
+
+# Install git dependencies
+sudo apt install -y git subversion zsh unity-tweak-tool
+
+# Install paper Icon theme
+sudo apt-get install -y paper-icon-theme
+
+# Install black theme
+mkdir -p $HOME/.themes
+cd $HOME/.themes
+git clone https://github.com/vooze/arc-black-ubuntu .
