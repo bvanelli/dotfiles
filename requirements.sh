54dd8be0df54f0d304b3c4fb5de3d4471ec59f3c
Brunno Vanelli <brunnovanelli@gmail.com>
2018-04-29 15:55:03 -0300

    Changed requirements to new theme.

--- requirements.sh
+++ requirements.sh
@@ -1,15 +1,18 @@
 # Add ppas
 sudo add-apt-repository ppa:snwh/pulp
-sudo apt-get update
 
 # Install git dependencies
-sudo apt install -y curl git subversion zsh unity-tweak-tool
+sudo apt install -y curl git subversion zsh gnome-tweak-tool chrome-gnome-shell
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install paper Icon theme
 sudo apt-get install -y paper-icon-theme
 
 # Install black theme
-mkdir -p $HOME/.themes
-cd $HOME/.themes
-git clone https://github.com/vooze/arc-black-ubuntu .
+mkdir git && cd git
+git clone https://github.com/vinceliuice/Yosemite-gtk-theme.git
+cd Yosemite-gtk-theme
+./Install
+
+# prompt install dash to panel
+firefox https://extensions.gnome.org/extension/1160/dash-to-panel/
